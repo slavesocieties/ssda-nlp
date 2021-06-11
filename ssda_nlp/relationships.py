a8d5b8bfa91250fea5c1076fa9e39b9aa1db1cb5
KDMalc <44472079+KDMalc@users.noreply.github.com>
2021-06-11 19:08:49 -0400

    Successfully identifying unassigned relationships

--- ssda_nlp/relationships.py
+++ ssda_nlp/relationships.py
@@ -392,8 +392,10 @@ def alt_assign_relationships(entry_text, entities, people_df, people, volume_met
                     #Initial attempt: add a column to entities, just binary whether
                     #Note that this relies on setting ALL to FOUND by default, so I don't have to add to the code above each time
                     #Thus, we only flip it in the case that no relationships are found
-                    entities['assgnmt_status'][index] = False
-                    #print("Failed to find a category for entity: " + entities["pred_entity"][index])
+
+                    #entities['assgnmt_status'][index] = False
+                    entities.iloc[6,index] = False
+                    print("Failed to find a category for relationship: " + entities["pred_entity"][index])
 
 
 
